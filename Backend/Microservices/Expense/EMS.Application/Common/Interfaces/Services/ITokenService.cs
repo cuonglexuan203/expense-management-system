0822e990cad80345445ebf45599f0807e6c710c6
cuonglexuan203 <cuonglexuan203@gmail.com>
2025-02-25 08:33:05 +0700

    add jwtservice

---
+++ Backend/Microservices/Expense/EMS.Application/Common/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using EMS.Core.Entities;
+using System.Security.Claims;
+
+namespace EMS.Application.Common.Interfaces.Services
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(IUser<string> user);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetClaimsPrincipalFromExpiredToken(string token);
+    }
+}
---
+++ Backend/Microservices/Expense/EMS.Infrastructure/Services/JwtService.cs
@@ -0,0 +1,79 @@
+﻿using EMS.Application.Common.Interfaces.Services;
+using EMS.Core.Entities;
+using EMS.Infrastructure.Common.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EMS.Infrastructure.Services
+{
+    public class JwtService : ITokenService
+    {
+        private readonly JwtSettings _settings;
+
+        public JwtService(IOptions<JwtSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public string GenerateAccessToken(IUser<string> user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_settings.SecretKey);
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, user.Id),
+                new(JwtRegisteredClaimNames.Email, user.Email),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_settings.AccessTokenExpirationInMinutes),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _settings.Issuer,
+                Audience = _settings.Audience,
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        public ClaimsPrincipal GetClaimsPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParams = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.SecretKey)),
+                ValidateLifetime = false,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParams, out var securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.OrdinalIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+        }
+    }
+}
