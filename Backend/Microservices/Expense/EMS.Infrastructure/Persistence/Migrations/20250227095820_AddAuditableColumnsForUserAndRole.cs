20c8661367aa640a7f67fd685e7b24512e6397e7
cuonglexuan203 <cuonglexuan203@gmail.com>
2025-02-27 16:59:17 +0700

    add migratoin for adding auditable columns of user and role

---
+++ Backend/Microservices/Expense/EMS.Infrastructure/Persistence/Migrations/20250227095820_AddAuditableColumnsForUserAndRole.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditableColumnsForUserAndRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "AspNetRoles",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "AspNetRoles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "AspNetRoles",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "AspNetRoles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetRoles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ModifiedAt",
+                table: "AspNetRoles",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "AspNetRoles",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "AspNetRoles");
+        }
+    }
+}
