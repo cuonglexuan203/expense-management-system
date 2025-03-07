2a2818d13e30991fb9099e952ede864e24e98580
cuonglexuan203 <cuonglexuan203@gmail.com>
2025-03-08 02:29:07 +0700

    fix config can not read env variables from .env file

--- Backend/Microservices/AI/app/core/config.py
+++ Backend/Microservices/AI/app/core/config.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -20,6 +20,10 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: str = ""
     GROQ_API_KEY: str = ""
     GOOGLE_API_KEY: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+    )
 
 
 settings = Settings()
