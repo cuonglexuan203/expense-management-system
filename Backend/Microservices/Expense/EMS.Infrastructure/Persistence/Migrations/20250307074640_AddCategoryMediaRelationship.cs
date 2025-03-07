57a329904800d66a6e37d41bd2715fe3cdbe73c4
Bui Quoc Thong <quocthong051@gmail.com>
2025-03-07 14:53:01 +0700

    create Add Category and Media relationship migration

---
+++ Backend/Microservices/Expense/EMS.Infrastructure/Persistence/Migrations/20250307074640_AddCategoryMediaRelationship.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryMediaRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IconId",
+                table: "Categories",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_IconId",
+                table: "Categories",
+                column: "IconId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Media_IconId",
+                table: "Categories",
+                column: "IconId",
+                principalTable: "Media",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Media_IconId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_IconId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IconId",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
