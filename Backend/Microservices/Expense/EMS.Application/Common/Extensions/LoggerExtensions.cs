f5cc2cdc9fdb516dd940e602a503673425d4da9b
cuonglexuan203 <cuonglexuan203@gmail.com>
2025-03-12 22:50:13 +0700

    add app state templates (logging)

---
+++ Backend/Microservices/Expense/EMS.Application/Common/Extensions/LoggerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using EMS.Core.Constants;
+using Microsoft.Extensions.Logging;
+
+namespace EMS.Application.Common.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogStateInfo(this ILogger logger, string state, string message)
+        {
+            logger.LogInformation(LogTemplates.AppStateChange, state, message);
+        }
+
+        public static void LogStateWarning(this ILogger logger, string state, string message)
+        {
+            logger.LogWarning(LogTemplates.AppStateChange, state, message);
+        }
+        public static void LogStateError(this ILogger logger, string state, string message, Exception? exception = null)
+        {
+            logger.LogError(exception, LogTemplates.AppStateChange, state, message);
+        }
+    }
+}
