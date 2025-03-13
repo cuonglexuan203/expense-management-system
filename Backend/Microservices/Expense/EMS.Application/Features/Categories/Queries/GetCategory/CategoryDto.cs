79737d070d05edcccc64c656e815bbb5d9757c45
cuonglexuan203 <cuonglexuan203@gmail.com>
2025-03-14 04:00:29 +0700

    fix can not set financial flow type for category

--- Backend/Microservices/Expense/EMS.Application/Features/Categories/Queries/GetCategory/CategoryDto.cs
+++ Backend/Microservices/Expense/EMS.Application/Features/Categories/Queries/GetCategory/CategoryDto.cs
@@ -4,19 +4,19 @@ using EMS.Core.Enums;
 
 namespace EMS.Application.Features.Categories.Queries.GetCategory
 {
-    public class CategoryDto : IMapFrom<Core.Entities.Category>
+    public class CategoryDto : IMapFrom<Category>
     {
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public int TransactionCount { get; set; }
         public bool IsDefault { get; set; }
-        public TransactionType? TransactionType { get; set; }
+        public TransactionType? FinancialFlowType { get; set; }
         public Guid? IconId { get; set; }
         public string? IconUrl { get; set; }
 
         public void Mapping(AutoMapper.Profile profile)
         {
-            profile.CreateMap<Core.Entities.Category, CategoryDto>()
+            profile.CreateMap<Category, CategoryDto>()
                 .ForMember(d => d.TransactionCount, opt => opt.MapFrom(s => s.Transactions.Count))
                 .ForMember(d => d.IsDefault, opt => opt.MapFrom(s => s.Type == CategoryType.Default))
                 .ForMember(d => d.IconUrl, opt => opt.MapFrom(s => s.Icon != null ? s.Icon.Url : null));
