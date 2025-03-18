ef643ce360971f0df65f88681c2636d1a1681694
cuonglexuan203 <cuonglexuan203@gmail.com>
2025-03-19 04:45:55 +0700

    add user preference for seed user accounts

--- Backend/Microservices/Expense/EMS.Application/Common/Interfaces/Services/IUserPreferenceService.cs
+++ Backend/Microservices/Expense/EMS.Application/Common/Interfaces/Services/IUserPreferenceService.cs
@@ -4,6 +4,7 @@ namespace EMS.Application.Common.Interfaces.Services
 {
     public interface IUserPreferenceService
     {
-        Task<UserPreference> GetUserPreferenceAsync();
+        Task<UserPreference> GetUserPreferenceAsync(CancellationToken cancellationToken = default);
+        Task CreateUserPreferencesAsync(string userId, CancellationToken cancellationToken = default);
     }
 }
